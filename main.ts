1e7581cc361e08afe2f788aa4387447ebd8c2935
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-05-07 16:50:10 +0200

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,27 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+/**
+ * Fügt der Eingabe einen einfach auszulesenden Mikrofonblock bei.
+ */
+namespace input {   // frech in input einklinken
+    /**
+     * Lautstärke in 0-100%
+     */
+
+    //% blockId="loudness"
+    //% block="Lautstärke"
+
+    export function loudness(): number {
+        let value = 0
+        let max = 0
+        let min = 1023
+        for (let index = 0; index < 32; index++) {
+            value = pins.analogReadPin(50)
+            if (value > max) {
+                max = value
+            } else if (value < min) {
+                min = value
+            }
+        }
+        value = (max - min) * 977 / 10000
+        return value
+    }
+}
